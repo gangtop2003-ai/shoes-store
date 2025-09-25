10dafa69c01e47b9d1ba04491f6f23660c46b8bf
gangtop2003-ai <gangtop2003@gmail.com>
2025-09-25 21:31:21 +0330

    init project

--- src/App.js
+++ src/App.js
@@ -6,27 +6,31 @@ import axios from "axios";
 import * as yup from "yup";
 
 function App() {
-  const[sabad,setsabad]=useState([])
+  const [sabad, setsabad] = useState([]);
   const Home = () => {
     return <div className="home-page">hello world</div>;
   };
-  const Bags=()=>{
-    return(
-     <div className="div_p1">
-      <div className="d1" style={{textAlign:'center'}}>
-        <h1 style={{color:'yellow'}}>کفش مجلسی زنانه</h1>
-        <a href="Bags_maj" ><img src="shoes.jpg"/></a></div>
-      <div className="d2">
-        <h1 style={{color:'yellow'}}>کفش اسپورت</h1>
-        <a href="Bags_sport"><img src="sport.jpg"/></a></div>
-     </div>
-    )
-  }
-  const Notfund=()=>{
-    return(
-      <div className="not"></div>
-    )
-  }
+  const Bags = () => {
+    return (
+      <div className="div_p1">
+        <div className="d1" style={{ textAlign: "center" }}>
+          <h1 style={{ color: "yellow" }}>کفش مجلسی زنانه</h1>
+          <a href="Bags_maj">
+            <img src="shoes.jpg" />
+          </a>
+        </div>
+        <div className="d2">
+          <h1 style={{ color: "yellow" }}>کفش اسپورت</h1>
+          <a href="Bags_sport">
+            <img src="sport.jpg" />
+          </a>
+        </div>
+      </div>
+    );
+  };
+  const Notfund = () => {
+    return <div className="not"></div>;
+  };
   const Bags_maj = () => {
     const bags_data = [
       {
@@ -58,7 +62,8 @@ function App() {
         name: "کفش غشی",
         img: "./shoes/s6.jpg",
         price: 1900,
-      },{
+      },
+      {
         name: " کفش 7 سانت فقلش",
         img: "./shoes/s2.jpg",
         price: 2899,
@@ -71,7 +76,7 @@ function App() {
       {
         name: "کفش جدید نشریغ",
         img: "./shoes/s1.jpg",
-        price:3000,
+        price: 3000,
       },
       {
         name: "کیف دستی چرمی",
@@ -88,7 +93,6 @@ function App() {
         img: "./shoes/s2.jpg",
         price: 2000,
       },
-
     ];
     return (
       <div className="bags-page">
@@ -99,64 +103,74 @@ function App() {
               <div className="div_img">
                 <img src={e.img} />
               </div>
-              <h2 style={{color:'black',fontFamily:'tahoma'}}>{e.price}</h2><br/>
-              <button onClick={()=>{ 
-                setsabad([...sabad,e])
-                console.log(sabad)
-              }}>خرید</button>
+              <h2 style={{ color: "black", fontFamily: "tahoma" }}>
+                {e.price}
+              </h2>
+              <br />
+              <button
+                onClick={() => {
+                  setsabad([...sabad, e]);
+                  console.log(sabad);
+                }}
+              >
+                اضافه کردن به سبد خرید
+              </button>
             </div>
           );
         })}
       </div>
     );
   };
-  const Sabad=()=>{
-    const[price,setprice]=useState(0)
-    useEffect(()=>{
-      console.log(sabad)
-      let m=0
-      const lk=sabad.map((e)=>{
-        return e.price
-      })
-      console.log(lk)
-      for(let i of lk){
-        m+=i
+  const Sabad = () => {
+    const [price, setprice] = useState(0);
+    useEffect(() => {
+      console.log(sabad);
+      let m = 0;
+      const lk = sabad.map((e) => {
+        return e.price;
+      });
+      console.log(lk);
+      for (let i of lk) {
+        m += i;
       }
-      setprice(m)
-     
+      setprice(m);
+    }, [sabad]);
 
-    },[sabad])
-   
-    return(
+    return (
       <>
- <div className="bags-page">
-        {sabad.map((e) => {
-          return (
-            <div className="bags-card">
-              <h2>{e.name}</h2>
-              <div className="div_img">
-                <img src={e.img} />
+        <div className="bags-page">
+          {sabad.map((e) => {
+            return (
+              <div className="bags-card">
+                <h2>{e.name}</h2>
+                <div className="div_img">
+                  <img src={e.img} />
+                </div>
+                <h2 style={{ color: "black", fontFamily: "tahoma" }}>
+                  {e.price}
+                </h2>
+                <br />
+                <button
+                  onClick={() => {
+                    const qw = structuredClone(sabad);
+                    const m = qw.filter((q) => {
+                      return e.name !== q.name;
+                    });
+                    setsabad(m);
+                  }}
+                >
+                  حذف
+                </button>
               </div>
-              <h2 style={{color:'black',fontFamily:'tahoma'}}>{e.price}</h2><br/>
-               <button onClick={()=>{ 
-                const qw=structuredClone(sabad)
-                const m=qw.filter((q)=>{
-                  return e.name!==q.name
-                })
-               setsabad(m)
-              }}>حذف</button>
-            </div>
-
-          );
-        })}
-      </div>
-      <div className="factor">
-        <h1>جمع کل قیمت:{price}</h1>
-      </div>
-     </>
-
-    )
-  }
+            );
+          })}
+        </div>
+        <div className="factor">
+          <h1>جمع کل قیمت:{price}</h1>
+        </div>
+      </>
+    );
+  };
   const Shoes = () => <h2>کفش مجلسی</h2>;
   return (
     <>
@@ -165,9 +179,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/bags" element={<Bags />} />
         <Route path="/shoes" element={<Shoes />} />
-        <Route path="/bags_maj" element={<Bags_maj/>} />
-        <Route path="*" element={<Notfund/>} />
-        <Route path="/sabad" element={<Sabad/>} />
+        <Route path="/bags_maj" element={<Bags_maj />} />
+        <Route path="*" element={<Notfund />} />
+        <Route path="/sabad" element={<Sabad />} />
       </Routes>
     </>
   );
