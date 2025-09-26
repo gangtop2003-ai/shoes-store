5ec37582efcd07958081fcec6b8a6d6362d26f98
gangtop2003-ai <gangtop2003@gmail.com>
2025-09-27 01:36:38 +0330

    vs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 import App from "./App";
 import ReactDOM from "react-dom/client";
 ReactDOM.createRoot(document.getElementById('root')).render(
