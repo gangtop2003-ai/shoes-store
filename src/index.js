ca244ba665525b797c773e21fee6fccf4d36b1b0
gangtop2003-ai <gangtop2003@gmail.com>
2025-09-27 01:12:10 +0330

    pk

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import App from "./App";
 import ReactDOM from "react-dom/client";
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App/>
     </BrowserRouter>
 )
